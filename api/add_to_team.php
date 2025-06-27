f7928166c70f03864501b8938ad6adb21b69148c
Faith Kalendek <FaithAEKalendek@gmail.com>
2025-06-26 21:55:30 -0400

    Changed the schema again (storing modifiers to multiply stats by for buff/debuff to make calculations easier)

--- api/add_to_team.php
+++ api/add_to_team.php
@@ -18,26 +18,26 @@ $pokemon_id = $_GET['pokemon_id'];
 // Add the pokemon to the character's team (initialize stats w/ data from pokemon table)
 if (isset($player_id)) {
     $query = $pdo->prepare("
-    INSERT INTO PlayerPokemon (
-        player_id, pokemon_id, current_hp, current_attack, current_defense, current_speed
-    )
-    SELECT
-        :player_id, id, max_hp, attack, defense, speed
-    FROM Pokemon
-    WHERE id = :pokemon_id
+        INSERT INTO PlayerPokemon (
+            player_id, pokemon_id, max_hp, current_hp, attack, attack_modifier, defense, defense_modifier, speed, speed_modifier, accuracy_debuff
+        )
+        SELECT
+            :player_id, id, max_hp, max_hp, attack, 1, defense, 1, speed, 1, 1
+        FROM Pokemon
+        WHERE id = :pokemon_id
     ");
-    $query->execute([":player_id"=> $player_id,":pokemon_id"=> $pokemon_id]);
+    $query->execute([":player_id"=> $player_id, ":pokemon_id"=> $pokemon_id]);
 } else {
     $query = $pdo->prepare("
-    INSERT INTO OpponentPokemon (
-        opponent_id, pokemon_id, current_hp, current_attack, current_defense, current_speed
-    )
-    SELECT
-        :opponent_id, id, max_hp, attack, defense, speed
-    FROM Pokemon
-    WHERE id = :pokemon_id
+        INSERT INTO OpponentPokemon (
+            opponent_id, pokemon_id, max_hp, current_hp, attack, attack_modifier, defense, defense_modifier, speed, speed_modifier, accuracy_debuff
+        )
+        SELECT
+            :opponent_id, id, max_hp, max_hp, attack, 1, defense, 1, speed, 1, 1
+        FROM Pokemon
+        WHERE id = :pokemon_id
     ");
-    $query->execute([":opponent_id"=> $opponent_id,":pokemon_id"=> $pokemon_id]);
+    $query->execute([":opponent_id"=> $opponent_id, ":pokemon_id"=> $pokemon_id]);
 }
 
 ?>
