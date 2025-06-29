f8e05731e1e9bad013750b957c8e990d692f9c8d
jmckinn2921 <jmckinn1@umbc.edu>
2025-06-28 22:10:56 -0400

    Added intial game state management and added gamestate.js file that helps manage the game state in proj3.js

---
+++ gamestate.js
@@ -0,0 +1,15 @@
+
+// This file defines / decides what game state the player is in.
+
+// Game states
+export const Phase = { 
+    TITLE : 'TITLE', 
+    OVERWORLD : "OVERWORLD", 
+    BATTLE : 'BATTLE',
+    CATCH : 'CATCH', 
+    RESULT : 'RESULT'
+};
+
+
+
+
